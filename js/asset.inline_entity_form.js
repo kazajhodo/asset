9f8e2195b686d0b072a89832cc75989dba550dff
Cyle Carlson <jacerider@gmail.com>
2013-11-08 08:51:59 -0600

    Initial commit.

---
+++ js/asset.inline_entity_form.js
@@ -0,0 +1,24 @@
+(function($) {
+
+Drupal.behaviors.asset_inline_entity_form = {
+  attach: function (context, settings) {
+    var self = this;
+    // $('.field-widget-inline-entity-form .asset-select').once(function(){
+    //   $(this).click( self.assetSelect ).closest('td').wrapInner('<div class="asset" />');
+    // });
+    $('.asset-inline-entity-browser .asset-select').once().click( self.assetSelect );
+  },
+
+  assetSelect: function ( e ) {
+    var self = this;
+    e.preventDefault();
+    var $this = $(this);
+    var id = $this.attr('data-id');
+    var $wrapper = $this.closest('.fieldset-wrapper');
+    var parents = $('.parents',$wrapper).val();
+    $('.asset-inline-entity',$wrapper).val(id);
+    $('input.ief-entity-submit',$wrapper).trigger('mousedown');
+  }
+}
+
+})(jQuery);
---
+++ modules/instance/plugins/instance/abstract.class.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Abstraction of the selection logic of a Mee instance.
+ *
+ * Implementations that wish to provide an implementation of this should
+ * register it using CTools' plugin system.
+ */
+interface AssetInstanceHandler {
+
+  public function __construct($type, $args = NULL);
+
+  public function instance_form(&$form, &$form_state, $settings, $defaults);
+
+}
+
+/**
+ * An abstract implementation of ButtonsReference_LinksHandler.
+ */
+abstract class AssetInstanceHandler_Abstract implements AssetInstanceHandler {
+
+  /**
+   * The name of the button plugin.
+   */
+  protected $type;
+
+  /**
+   * The plugin definition.
+   */
+  protected $plugin;
+
+  /**
+   * Constructor for the links.
+   *
+   * @param $type
+   * The name of the links plugin.
+   */
+  public function __construct($type, $args = NULL) {
+    $this->type = $type;
+    ctools_include('plugins');
+    $plugin = ctools_get_plugins('mee', 'instance', $type);
+    $this->plugin = $plugin;
+  }
+
+  public function defaults($settings) {}
+
+  public function settings_form(&$form, &$form_state, $defaults) {}
+
+  public function instance_form(&$form, &$form_state, $settings, $defaults) {}
+
+  public function instance_render(&$vars, $settings, $config) {}
+
+  public function instance_render_preview(&$vars, $settings, $config) {
+    $this->instance_render($vars, $settings, $config);
+  }
+
+}
+
+/**
+ * A null implementation of Mee_LinksHandler.
+ */
+class AssetInstance_InstanceHandler_Broken extends AssetInstanceHandler_Abstract {
+
+}
