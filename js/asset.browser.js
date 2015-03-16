a7228905f324b086919f1fe1a6afe8593474e4f4
jacerider <jacerider@gmail.com>
2015-03-16 12:25:12 -0500

    Remove console.

--- js/asset.browser.js
+++ js/asset.browser.js
@@ -14,7 +14,6 @@ Drupal.behaviors.asset_browser = {
     var id = $this.attr('data-id');
     var $wrapper = $this.closest('.asset-inline-entity-browser');
     var parents = $('.parents',$wrapper).val();
-    console.log($wrapper);
     $('.asset-inline-entity',$wrapper).val('Asset (' + id + ')');
     $('input.ief-entity-submit,button.ief-entity-submit',$wrapper).trigger('mousedown');
   }
