357e182873629103d842ff5e82d6559e1fb26613
JaceRider <jacerider@gmail.com>
2015-08-17 15:41:46 -0500

    Rename name column to title for better compatability. Add status column.

--- plugins/widget/assetWidgetPlaceholder.class.php
+++ plugins/widget/assetWidgetPlaceholder.class.php
@@ -7,7 +7,7 @@ class assetWidgetPlaceholder extends assetWidget {
     $vars['content'] = array();
     $vars['content']['#attached']['css'][] = drupal_get_path('module', 'asset') . '/plugins/widget/css/assetWidgetPlaceholder.css';
     $vars['content']['placeholder'] = array(
-      '#markup' => '<div class="asset-placeholder"><div class="fa fa-'.$asset_type->data['icon'].'"><div class="asset-placeholder-hide">&nbsp;</div></div>&nbsp;&nbsp;<strong>'.$asset_type->name.':</strong> '.$vars['asset']->name.'</div>',
+      '#markup' => '<div class="asset-placeholder"><div class="fa fa-'.$asset_type->data['icon'].'"><div class="asset-placeholder-hide">&nbsp;</div></div>&nbsp;&nbsp;<strong>'.$asset_type->title.':</strong> '.$vars['asset']->name.'</div>',
     );
     // We always want to fall back to default tpl file when previewing.
     $vars['theme_hook_suggestions'] = array('asset');
