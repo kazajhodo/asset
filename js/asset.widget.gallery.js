13f799cbff11ab3780e4f2c78e92368ca0849477
JaceRider <jacerider@gmail.com>
2016-02-19 09:07:28 -0600

    Asset gallery now support video.

--- js/asset.widget.gallery.js
+++ js/asset.widget.gallery.js
@@ -4,16 +4,36 @@ Drupal.behaviors.asset_widget_gallery = {
   attach: function(context, settings) {
     $('.asset-gallery', context).once(function(){
       var $this = $(this);
-      var $imageFull = $this.find('.asset-gallery-full img:first');
-      var $children = $this.find('.asset-gallery-thumbs a');
-      $children.first().addClass('active');
-      $children.on('click', function(e){
-        e.preventDefault();
-        var $image = $(this).find('img');
-        $children.removeClass('active');
-        $(this).addClass('active');
-        $imageFull.attr('src', $(this).attr('href'));
-      });
+      var galleryId = $this.data('asset-gallery');
+      if(settings.asset && settings.asset.gallery && settings.asset.gallery[galleryId]){
+        var galleryMarkup = settings.asset.gallery[galleryId];
+        var $imageFull = $this.find('.asset-gallery-full');
+        var $children = $this.find('.asset-gallery-thumbs a');
+        var timeout;
+        $children.first().addClass('active');
+        $children.on('click', function(e){
+          var $self = $(this);
+          var itemId = $self.data('asset-gallery-id');
+          e.preventDefault();
+          if(galleryMarkup[itemId]){
+            clearTimeout(timeout);
+            $imageFull.addClass('animate');
+            $children.removeClass('active');
+            $self.addClass('active');
+            var $markup = $(galleryMarkup[itemId]);
+            // Load included images first before switching content.
+            $markup.find('img').on('load', function(){
+              timeout = setTimeout(function(){
+                $imageFull.html($markup);
+                Drupal.attachBehaviors($imageFull);
+                timeout = setTimeout(function(){
+                  $imageFull.removeClass('animate');
+                },10);
+              }, 300);
+            });
+          }
+        });
+      }
     });
   }
 };
